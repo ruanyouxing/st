26e3ae523968204008e05b4d1af988464d60ed03
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-06 16:45:59 +0700

    h

--- config.def.h
+++ config.def.h
@@ -6,13 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+    "JetBrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
      */
-    "Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true",
-    "Iconsolate for Powerline:pixelsize=15:antialias=true:autohint=true",
+    "Iosevka Nerd Font:pixelsize=16:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
